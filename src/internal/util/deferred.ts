19655fedac821860ab795287144bc73c70ebb793
silence <silence_zhpf@aliyun.com>
2020-12-23 11:30:19 +0800

    Update deferred.ts

--- src/internal/util/deferred.ts
+++ src/internal/util/deferred.ts
@@ -1,7 +1,7 @@
 export class Deferred<T> {
   resolve: (value?: T | PromiseLike<T>) => void = null!;
   reject: (reason?: any) => void = null!;
-  promise = new Promise<T>((a, b) => {
+  promise = new Promise<T>((a: T | PromiseLike<T>, b) => {
     this.resolve = a;
     this.reject = b;
   });
